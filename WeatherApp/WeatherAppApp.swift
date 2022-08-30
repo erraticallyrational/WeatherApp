e059c7f27c31d61c71027a07408d9b500f33778c
Shaquil Campbell <shaquilwashington@gmail.com>
2022-08-30 19:26:53 -0400

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Shaquil Campbell on 8/30/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
